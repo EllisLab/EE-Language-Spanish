a0e466c2d387a2242cce464d4ab2383accd3ae26
westhouseit <github@westhouseit.co.uk>
2014-02-05 18:26:55 +0000

    Partial translation of missing lang file

---
+++ spanish/channel_form_lang.php
@@ -0,0 +1,34 @@
+<?php 
+
+$lang = array( 
+	'channel_form_no_channel' => 'No channel was specified.',
+	'channel_form_invalid_guest_member_id' => 'The provided member id to use for guest submissions is invalid.',
+	'channel_form_require_entry' => 'You must specify a valid entry to edit.',
+	'channel_form_author_only' => 'You are not the author of this entry.',
+	
+	/* rules */
+	'form_decryption_failed' => 'Decryption of form settings failed.',
+	'required' => 'Es obligatorio rellenar la casilla %s.',
+	'isset' => 'The %s field must have a value.',
+	'valid_email' => 'The %s field must contain a valid email address.',
+	'valid_emails' => 'The %s field must contain all valid email addresses.',
+	'valid_url' => 'The %s field must contain a valid URL.',
+	'valid_ip' => 'The %s field must contain a valid IP.',
+	'min_length' => 'The %s field must be at least %s characters in length.',
+	'max_length' => 'The %s field cannot exceed %s characters in length.',
+	'exact_length' => 'The %s field must be exactly %s characters in length.',
+	'alpha' => 'La casilla %s solo puede contener caracteres alfabéticos.',
+	'alpha_numeric' => 'La casilla %s solo puede contener caracteres alfanuméricos.',
+	'alpha_dash' => 'La casilla %s solo puede contener caracteres alfanuméricos, guiones bajos y guiones.',
+	'numeric' => 'La casilla %s solo puede contener números.',
+	'is_numeric' => 'The %s field must contain only numeric characters.',
+	'integer' => 'The %s field must contain an integer.',
+	'matches' => 'La casilla %s no coincide con la casilla %s.',
+	'is_natural' => 'The %s field must contain only positive numbers.',
+	'is_natural_no_zero' => 'The %s field must contain a number greater than zero.',
+	'valid_date' => 'The %s you submitted was not formatted correctly (year-month-day hour:minute)',
+);
+
+
+/* End of file channel_form_lang.php */
+/* Location: ./system/expressionengine/language/spanish/channel_form_lang.php */
